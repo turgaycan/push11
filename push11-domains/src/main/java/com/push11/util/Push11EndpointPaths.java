105c3d92c0dd382f4d93c4ea7a1e5b920fac5280
Turgay Can <turgay.can@n11.com>
2015-09-09 16:07:06 +0300

    haydaaa

--- push11-domains/src/main/java/com/push11/util/Push11EndpointPaths.java
+++ push11-domains/src/main/java/com/push11/util/Push11EndpointPaths.java
@@ -1,10 +1,13 @@
 package com.push11.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Push11EndpointPaths {
 
     private Push11EndpointPaths(){}
 
     //TODO turgay : Tüm end point path'lerini burada topla
+    public static final String EMPTY = StringUtils.EMPTY;
     public static final String VERSION = "/{version}";
     public static final String APPLICATION = "/application";
     public static final String V_APPLICATION = VERSION +  "/application";

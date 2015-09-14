e06398f1b7d1bc8d6da67e4b5d3b0037d087967d
onur taskin <onur.taskin@n11.com>
2015-09-14 14:09:10 +0300

    Added Action and Event repository

--- push11-data/src/main/java/com/push11/data/repository/ActionRepository.java
+++ push11-data/src/main/java/com/push11/data/repository/ActionRepository.java
@@ -2,6 +2,10 @@ package com.push11.data.repository;
 
 import com.push11.domain.Action;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface ActionRepository extends MongoRepository<Action, String> {
+
+    @Query(value = "{ 'actionGroupId': ?0 ,'user.r_id':?1} ")
+    public Action getActionFindByActionAndRegId(String actionGroupId, String regId);
 }
--- push11-data/src/main/java/com/push11/data/repository/EventRepository.java
+++ push11-data/src/main/java/com/push11/data/repository/EventRepository.java
@@ -2,6 +2,15 @@ package com.push11.data.repository;
 
 import com.push11.domain.Event;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface EventRepository extends MongoRepository<Event, String> {
+
+    @Query("{'user.app._id' : ?0}")
+    public List<Event> getEventsFindByApp(String appId);
+
+    @Query(value = "{ 'user.app._id': ?0 ,'event_name':?1} ")
+    public List<Event> getEventsFindByAppAndEventName(String appId, String eventName);
 }
